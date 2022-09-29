f893b71ebc9d6b7c64cdae992aadcc81c8908d3d
ocmino <ocmino@gmail.com>
2022-09-29 11:53:40 +0200

    add footer

--- src/Models/Shoe/Shoe.js
+++ src/Models/Shoe/Shoe.js
@@ -21,16 +21,16 @@ const style = {
 
 const buttonStyle = {
   position: "absolute",
-  top: "50%",
+  top: "0",
   left: "0",
   right: "0",
   bottom: "0",
   margin: "auto",
-  width: "150px",
+  width: "100px",
   height: "100px",
-  background: "gray",
+  background: "transparent",
   border: "none",
-  color: "black",
+  color: "white",
   fontSize: "20px",
   fontWeight: "bold",
   cursor: "pointer",
@@ -40,7 +40,7 @@ const buttonStyle = {
 
   return (
 
-   /*  <model-viewer
+    <model-viewer
       src="Assets/shoe.glb"
       alt="A 3D model of an astronaut"
       ar
@@ -53,31 +53,7 @@ const buttonStyle = {
       environment-image="neutral"
       exposure="1"
       style={style}  
-    ></model-viewer> */
-
-    <div>
-      <model-viewer
-        src="Assets/shoe.glb"
-        alt="A 3D model of an astronaut"
-        camera-controls
-        auto-rotate
-        shadow-intensity="1"
-        environment-image="neutral"
-        exposure="1"
-        style={style}
-      ></model-viewer>
-      <button style={buttonStyle}
-      //view in ar onClick with ar-scale, ar-placement, ar-modes, camera-controls, auto-rotate, shadow-intensity, environment-image, exposure
-      onClick={() => {
-        const modelViewer = document.querySelector("model-viewer");
-        modelViewer.setAttribute("ar", true);
-        modelViewer.setAttribute("ar-scale", "auto");
-        modelViewer.setAttribute("ar-placement", "floor");
-        modelViewer.setAttribute("ar-modes", "scene-viewer quick-look webxr");
-
-      }}
-      >View in AR</button>
-    </div>
+    ></model-viewer>
     
   
 
